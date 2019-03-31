b16c8af90b9f4ad97d81b8ad90b7e88cb7c331a1
Bruno Rodrigues <bruno@brodrigues.co>
2019-03-31 17:35:14 +0200

    first commit

---
+++ data-raw/get_leierchen.R
@@ -0,0 +1,29 @@
+#"http://www.luxemburgensia.bnl.lu/cgi/getPdf1_2.pl?mode=item&id=7110"
+
+library(tidyverse)
+library(tesseract)
+library(pdftools)
+library(magick)
+
+
+#pngfile <- pdf_convert("~/Downloads/dleierchen/dleierchen.pdf", dpi = 600)
+
+path <- dir(path = "~/Downloads/dleierchen", pattern = "*.png", full.names = TRUE)
+
+images <- map(path, image_read)
+
+first_half <- map(images, ~image_crop(., geometry = "2307x6462"))
+
+second_half <- map(images, ~image_crop(., geometry = "2307x6462+2307+0"))
+
+merged_list <- prepend(first_half, NA) %>%
+    reduce2(second_half, c) %>%
+    discard(is.na)
+
+text_list <- map(merged_list, ~ocr(., engine = "nld"))
+
+text_list <- text_list %>%
+    map(., ~str_split(., "\n"))
+
+text_list %>%
+    unlist %>% View
---
+++ data-raw/get_renert.R
@@ -0,0 +1,40 @@
+# this scripts gets the text from the "Renert" and puts it into a nice data frame
+
+library("tidyverse")
+library("tidytext")
+library("janitor")
+
+renert_link <- "https://wikisource.org/wiki/Renert"
+
+renert_raw <- renert_link %>%
+    xml2::read_html() %>%
+    rvest::html_nodes(".mw-parser-output") %>%
+    rvest::html_text() %>%
+    str_split("\n", simplify = TRUE) %>%
+    .[1, -c(1:21)]
+
+(indices <- grepl("Gesank", renert_raw) %>% which(isTRUE(.)))
+
+(indices2 <- c(indices[-1] - 1, length(renert_raw)))
+
+song_lines <- map2(indices, indices2,  ~seq(.x,.y))
+
+renert_songs <- map(song_lines, ~`[`(renert_raw, .))
+
+is_empty_character <- function(char){
+    if(char == "") TRUE else FALSE
+}
+
+renert <- renert_songs %>%
+    map(., ~discard(., is_empty_character)) %>%
+    map(., ~str_remove_all(., "\\[edit\\]")) %>%
+    map(tibble) %>%
+    map(., ~rename(., sentence = `<chr>`)) %>%
+    map(., ~mutate(., gesank = pull(.[1, 1]))) %>%
+    map(., ~mutate(., gesank = str_remove_all(gesank, " Gesank."))) %>%
+    map(., ~mutate(., gesank = tolower(gesank))) %>%
+    bind_rows()
+
+renert %>%
+    group_by(gesank) %>%
+    summarise(total_lines = n())
